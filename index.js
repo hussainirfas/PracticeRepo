a6b32e94a3c67d8e351aacd17a5813382e613b1d
hussainirfas <hussainirfas@gmail.com>
2022-11-08 14:05:29 +0530

    Ok

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // XMLHttpRequest => is an object in our browser => for AJAX calls
 var XMLHttpRequest = require('xhr2')
 
+function reqListener(){
+    console.log(this.responseText); 
+}
+
 // Step 1: Instantiate this Object
 var req = new XMLHttpRequest();
 req.addEventListener("load", reqListener)
